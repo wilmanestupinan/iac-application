2d5b6a587b1cbf403f5637cae117b57dcaeaf1d1
cap-wilman-01 <cap.wilman.01@gmail.com>
2025-01-18 21:47:25 -0500

    Create vpc with cdk

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ IacApplicationStack(app, "IacApplicationStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='767397670732', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
